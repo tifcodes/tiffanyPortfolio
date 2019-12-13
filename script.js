da58f0c65bafe5412a24b12eed3af9712a7c41de
Tiffany <tif.wong@yahoo.com>
2019-12-13 12:32:57 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let app = {};
+
+app.init = function() {
+    $( ".cross" ).hide();
+    $( ".menu" ).hide();
+
+    $( ".hamburger" ).click(function() {
+        $( ".menu" ).slideToggle( "slow", function() {
+        $( ".hamburger" ).hide();
+        $( ".cross" ).show();
+        });
+    });
+    
+    $( ".cross" ).click(function() {
+        $( ".menu" ).slideToggle( "slow", function() {
+        $( ".cross" ).hide();
+        $( ".hamburger" ).show();
+        });
+    });
+}
+
+$(function() {
+    app.init();
+});
