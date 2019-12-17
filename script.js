a252a9f34dbac2760b70b72daee5a3d4d6efe9e1
Tiffany <tif.wong@yahoo.com>
2019-12-17 12:18:45 -0500

    added favicon

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
 let app = {};
 
-app.init = function() {
-    $( ".cross" ).hide();
-    $( ".menu" ).hide();
+app.init = function () {
+    $(".cross").hide();
+    $(".menu").hide();
 
-    $( ".hamburger" ).click(function() {
-        $( ".menu" ).slideToggle( "slow", function() {
-        $( ".hamburger" ).hide();
-        $( ".cross" ).show();
-        });
-    });
-    
-    $( ".cross" ).click(function() {
-        $( ".menu" ).slideToggle( "slow", function() {
-        $( ".cross" ).hide();
-        $( ".hamburger" ).show();
+    $(".hamburger").click(function () {
+        $(".menu").slideToggle("slow", function () {
+            $(".hamburger").hide();
+            $(".cross").show();
         });
     });
 
-    $( ".hamburger" ).keypress(function() {
-        $( ".menu" ).slideToggle( "slow", function() {
-        $( ".hamburger" ).hide();
-        $( ".cross" ).show();
+    $(".cross").click(function () {
+        $(".menu").slideToggle("slow", function () {
+            $(".cross").hide();
+            $(".hamburger").show();
         });
     });
-    
-    $( ".cross" ).keypress(function() {
-        $( ".menu" ).slideToggle( "slow", function() {
-        $( ".cross" ).hide();
-        $( ".hamburger" ).show();
+
+    $(".hamburger").keypress(function () {
+        $(".menu").slideToggle("slow", function () {
+            $(".hamburger").hide();
+            $(".cross").show();
+        });
+    });
+
+    $(".cross").keypress(function () {
+        $(".menu").slideToggle("slow", function () {
+            $(".cross").hide();
+            $(".hamburger").show();
         });
     });
 }
 
-$(function() {
+$(function () {
     app.init();
     AOS.init();
-    $(function () {
-        $('a').smoothScroll();
-    });
+    $('a').smoothScroll();
+    if (window.innerWidth > 800) {
+        $("h1").addClass("noanimated");
+    }
 });
